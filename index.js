b5c5c4062e972de54941e3befb43ac85f9c7000e
admacas <admacas@espol.edu.ec>
2021-08-21 20:21:32 -0500

    servidor code change

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(cors());
 // Habilitar express.json
 app.use(express.json({extended:true})); 
 // creando un puerto para la comunicacion con el servidor
-const port = process.env.port || 4000;
+const PORT = process.env.PORT || 4000;
 // Definir la pagina principal
 /* app.get('/',(req,res) => {
     res.send('Hola mundo')
@@ -23,6 +23,6 @@ app.use('/api/auth',require('./routes/auth'));
 app.use('/api/proyectos',require('./routes/proyectos'));
 app.use('/api/tareas',require('./routes/tareas'));
 // arrancar la app
-app.listen(port,'0.0.0.0',() => {
-    console.log(`El servidor esta funcionando en el puerto ${port}`);
+app.listen(PORT,'0.0.0.0',() => {
+    console.log(`El servidor esta funcionando en el puerto ${PORT}`);
 }); 
